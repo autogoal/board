459d8510205092e09e4c011c65305358a676910f
LazaroDGM <66334895+LazaroDGM@users.noreply.github.com>
2020-09-06 18:44:26 -0400

    Add generator mode selector

--- src/generate.py
+++ src/generate.py
@@ -12,25 +12,37 @@ def main():
     path_generate = Path(__file__).parent.parent / "pipelines"
     path_generate.mkdir(exist_ok=True)    
 
+    select_mode = st.sidebar.selectbox("Select mode", ["Clone file"])
+
     name_file = st.text_input("New file's name", "new.jsonl")
-    overwrite = st.checkbox("Overwrite")
+    overwrite = st.checkbox("Overwrite")    
+
+    path_file = path_generate / name_file
+
+    if path_file.exists() and not overwrite:
+        st.error("If file exists, then confirm **Overwrite** Option")
+        return
+
+    if select_mode == "Clone file":
+        clone_mode(path_file)
+
+
+def clone_mode(path_file):
 
     original_file = st.file_uploader("Input original file")
     time_sleep = st.number_input("Set sleep time",min_value=1, value=3, step=1)
 
-    edit_file = None
-
-    if st.button("Clone") and original_file:
-        if (path_generate / name_file).exists() and not overwrite:
-            st.error("If file exists, then confirm **Overwrite** Option")
-            return
-        edit_file = open(str(path_generate / name_file),"w")        
+    if st.button("Clone") and original_file:        
+        edit_file = open(str(path_file),"w")
+        file_view = st.empty()
 
         for s in re.findall("\{.*\}",original_file.read()):
-            edit_file.writelines(s + "\n")
+            text = s+"\n"
+            edit_file.write(text)
             edit_file.flush()
-            st.write(s)
-            sleep(time_sleep)
+            file_view.text_area("File",text, height=400)
+
+            sleep(time_sleep)    
     
 
 if __name__ == "__main__":
